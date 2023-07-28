8fe5a078342d5955737ad013d47dfc0679ee2953
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-07-28 15:57:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gbs-fwk-docs-next
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-tk2wn8)
---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>GBS FrameWork Docs</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'GBS Framework Documentation © 2023',
+  },
+}
+
+export default config
