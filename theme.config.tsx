132c72ab46e6c3f85b2fee0b37ae478d15c936c8
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-07-29 09:21:51 +0530

    1 file modified

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
   logo: <span>GBS FrameWork Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ananduremanan/gbs-fwk-docs-next',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ananduremanan/gbs-fwk-docs-next',
   footer: {
     text: 'GBS Framework Documentation © 2023',
   },
-}
+};
 
-export default config
+export default config;
